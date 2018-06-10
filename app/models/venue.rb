7c5aefc03580f332a53be3c1fdbdc7f2ecd324a7
Adam <ebokianknight@gmail.com>
2018-06-10 13:24:29 -0400

    Added Seeds and a master array of venues, nothing is attached yet.

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,5 +1,21 @@
 class Venue < ApplicationRecord
+  TYPES = [
+    'FAIR',
+    'FESTIVAL',
+    'THEATER',
+    'CORPORATE',
+    'PRIVATE',
+    'SUMMER CONCERT SERIES',
+    'SPEEDWAY',
+    'CASINO',
+    'PERFORMING ARTS CENTER',
+    'OPERA HOUSE',
+    'AUDITORIUM',
+    'OTHER'
+  ]
+
   validates :name, presence: true
+  validates :kind, allow_blank: true, inclusion: TYPES
   has_and_belongs_to_many :individuals
   has_and_belongs_to_many :organizations
   has_many :events
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Individual.delete_all
+Organization.delete_all
+Venue.delete_all
+Event.delete_all
+
+john = Individual.create(
+  first_name: "John",
+  last_name: "Doe",
+  job_title: "Manager",
+  phone: "(555)555-5555"
+)
+
+jane = Individual.create(
+  first_name: "Jane",
+  last_name: "Doe",
+  job_title: "VP",
+  phone: "(555)555-5555"
+)
+
+mario = Individual.create(
+  first_name: "Mario",
+  last_name: "Sunshine",
+  job_title: "Super",
+  phone: "(555)555-5555"
+)
+
+pipe = Organization.create(name: "Green Pipe")
+play = Organization.create(name: "Speedway")
+kupa = Venue.create(name: 'stuff', city: "Bowsers", state: "Toad Kingdom")
+toad = Venue.create(name: 'stuff', city: "Village", state: "Toad Kingdom")
+a_speedway = Venue.create(name: 'stuff', city: "Columbus", state: "OH")
+
+john.update(organizations: [play, pipe], venues: [kupa, a_speedway])
+mario.update(organizations: [pipe], venues: [kupa, toad])
+jane.update(organizations: [play], venues: [a_speedway])
+
+Event.create(name: 'Country Dance thing', date: 1.week.ago, comments: "A bunch of booze and music.", venue: a_speedway)
+Event.create(name: 'Country Dance II', date: 1.week.from_now, comments: "Some more bunch of booze and music.", venue: a_speedway)
